2af6b9f67dfbd40c22d38da66e538d87e8e886af
lorenamudure21 <115098143+lorenamudure21@users.noreply.github.com>
2024-10-16 22:52:27 +0300

    Delete spriint7/git directory

--- spriint7/git/tema2.js
+++
@@ -1,8 +0,0 @@
-//cream functia tellFortune care accepta cele 4 argumente rescrise in cerinta 
-function tellFortune(N,Z,Y,X){
-    // in acest consol.log cu ajutorul backtick-ului am reusit incliderea variabilelor cum am fi locul sau numarul de copiii direct in text 
-      console.log(`Vei fi un ${X} de succes, care va profesa in ${Y}, alaturi de minunata ta sotie ${Z} si cei ${N} copiii.`);
-  
-  }
-  //aici apelam functia tellFortune cu cele 4 argumente, adica N este nr de copiii, Z este numele persoanei, Y devine locul descris si X este profesia
-  tellFortune(2, "Victoria", "Paris", "doctor");
