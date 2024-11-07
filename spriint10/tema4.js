882a71b2f56ada17d851bb2920fafdd8df05cc6c
lorenamudure21 <lorenamudure21@gmail.com>
2024-11-07 21:28:08 +0200

    am adaugat sprint1

---
+++ spriint10/tema4.js
@@ -0,0 +1,8 @@
+// aici am definit funcția addNumber care primeste un numar nelimitat de argumente
+function addNumber(...numbers) {
+// am folosit metoda reduce pentru a calcula suma tuturor numerelor din sirul de numere
+        return numbers.reduce((sum, current) => sum + current, 0);
+}      
+console.log(addNumber(1, 2, 3, 4, 5)); 
+console.log(addNumber(10, 20, 30));     
+console.log(addNumber());
